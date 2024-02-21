40d92677ed96b561b2a0124ecba8df7d7972a125
Charuka Wijethunga <charukawi@masholdings.com>
2024-02-21 15:42:36 +0530

    update2 app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
-
+import xlsxwriter
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    workbook = xlsxwriter.Workbook('demo.xlsx')
     if request.method == 'GET':
         return render_template('index.html')
     
