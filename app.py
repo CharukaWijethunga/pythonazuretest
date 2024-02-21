13ddaefdee8e01bd12c1b6ad3990523814c62913
Charuka Wijethunga <charukawi@masholdings.com>
2024-02-21 13:59:11 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    if request.method == 'GET':
-        return render_template('index.html')
+    return render_template('index.html')
+        
     
 if __name__ == '__main__':
     app.run()
